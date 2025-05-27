36673436cab77fa769e38c0eab669082677b2433
sabjanukeraj <sabiukperaj@icloud.com>
2025-05-28 00:51:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server läuft erfolgreich!");
+  res.send("Backend läuft!");
 });
 
 app.listen(PORT, () => {
